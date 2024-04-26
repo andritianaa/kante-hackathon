edda7dd14aac719bd818e33ac3d9869a16d9be70
andritianaa <andritianasteve@gmail.com>
2024-04-26 09:07:31 +0300

    add props

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,34 @@ import { Hero } from "@/components/home/hero";
 import { NavBar } from "@/components/home/navbar";
 import { Feature } from "@/components/home/feature";
 import { Footer } from "../components/home/Footer";
+import { ChocolateCard } from "../components/offer/ChocolateCard";
+import { Chocolate } from "../types/chocolate";
 
 export default function Home() {
+  const chocolate = {
+    chocolat_id: 1,
+    nom: "Tablette Noir 70%",
+    description: "Delicieuse tablette de chocolat noir avec 70% de cacao.",
+    categorie: "Tablettes de chocolat",
+    origine: "C“te Ivoire",
+    prix: "38400.00",
+    image: "tablette_noir_70.png",
+  };
   return (
     <>
+      <ChocolateCard
+        chocolat_id={chocolate.chocolat_id}
+        nom={chocolate.nom}
+        description={chocolate.description}
+        categorie={chocolate.categorie}
+        origine={chocolate.origine}
+        prix={chocolate.prix}
+        image={chocolate.image}
+      />
       <NavBar />
       <Hero />
       <Feature />
       <Footer />
-
     </>
   );
 }
