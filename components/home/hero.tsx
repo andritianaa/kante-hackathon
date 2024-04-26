bb38b3c7f7121792e0b344fb74e4dcf220def08e
Manohisoaa <manohisoanirina707@mail.com>
2024-04-26 15:03:08 +0300

    commit sary card

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -16,7 +16,7 @@ export function Hero() {
         </div>
 
 
-        <div className="max-w-[85rem] px-4 sm:px-6 lg:px-8">
+        <div className="max-w-[85rem] mb-20 px-4 sm:px-6 lg:px-8">
           <div className="grid lg:grid-cols-7 lg:gap-x-8 xl:gap-x-12 lg:items-center">
             <div className="lg:col-span-3">
               <h1 className="block text-3xl font-bold text-gray-800 sm:text-4xl md:text-5xl lg:text-6xl dark:text-white">Edenia chocolaterie</h1>
@@ -59,6 +59,102 @@ export function Hero() {
             </div>
           </div>
         </div> */}
+
+        <section className="py-10 bg-gray-50 sm:py-16 lg:py-24">
+          <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+            <div className="flex items-end justify-between">
+              <div className="flex-1 text-center lg:text-left">
+                <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">Pourquoi nos produits?</h2>
+                <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-600 lg:mx-0">Nos produits se distinguent par Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quidem expedita officia ipsa unde accusantium</p>
+              </div>
+
+
+            </div>
+
+            <div className="grid max-w-md grid-cols-1 gap-6 mx-auto mt-8 lg:mt-16 lg:grid-cols-3 lg:max-w-full">
+              <div className="overflow-hidden bg-white rounded shadow">
+                <div className="p-5">
+                  <div className="relative">
+                    <a href="#" title="" className="block aspect-w-4 aspect-h-3">
+                      <img className="object-cover w-full h-full" src="/Pourquoi-manger-du-chocolat-n-est-pas-tres-ecolo.jpg" alt="" />
+                    </a>
+
+
+                  </div>
+                  <p className="mt-5 text-2xl font-semibold">
+                    <a href="#" title="" className="text-black"> Plantation de cacao bien entretenue </a>
+                  </p>
+                  <p className="mt-4 text-base text-gray-600">Dans nos plantations de cacao écologique, chaque geste est empreint de soin et de respect pour la nature. Les cultivateurs s'engagent à cultiver le cacao de manière durable, en utilisant des méthodes agroforestières qui préservent la biodiversité et régénèrent les sols.</p>
+                  <a href="#" title="" className="inline-flex items-center justify-center pb-0.5 mt-5 text-base font-semibold text-blue-600 transition-all duration-200 border-b-2 border-transparent hover:border-blue-600 focus:border-blue-600">
+                    Continue Reading
+                    <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+
+              <div className="overflow-hidden bg-white rounded shadow">
+                <div className="p-5">
+                  <div className="relative">
+                    <a href="#" title="" className="block aspect-w-4 aspect-h-3">
+                      <img className="object-cover w-full h-full" src="/P1010232_2.jpg" alt="" />
+                    </a>
+
+
+                  </div>
+                  <p className="mt-5 text-2xl font-semibold">
+                    <a href="#" title="" className="text-black"> Production suivant la norme </a>
+                  </p>
+                  <p className="mt-4 text-base text-gray-600">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                  <a href="#" title="" className="inline-flex items-center justify-center pb-0.5 mt-5 text-base font-semibold text-blue-600 transition-all duration-200 border-b-2 border-transparent hover:border-blue-600 focus:border-blue-600">
+                    Continue Reading
+                    <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+
+              <div className="overflow-hidden bg-white rounded shadow">
+                <div className="p-5">
+                  <div className="relative">
+                    <a href="#" title="" className="block aspect-w-4 aspect-h-3">
+                      <img className="object-cover w-full h-full" src="/emaballage-papier-de-cacao-le-chocolat-de-tiffanie.jpg" alt="" />
+                    </a>
+
+
+                  </div>
+                  <p className="mt-5 text-2xl font-semibold">
+                    <a href="#" title="" className="text-black"> 5 Productivity tips to write faster at morning. </a>
+                  </p>
+                  <p className="mt-4 text-base text-gray-600">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                  <a href="#" title="" className="inline-flex items-center justify-center pb-0.5 mt-5 text-base font-semibold text-blue-600 transition-all duration-200 border-b-2 border-transparent hover:border-blue-600 focus:border-blue-600">
+                    Continue Reading
+                    <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-center mt-8 space-x-3 lg:hidden">
+              <button type="button" className="flex items-center justify-center text-gray-400 transition-all duration-200 bg-transparent border border-gray-300 rounded w-9 h-9 hover:bg-blue-600 hover:text-white focus:bg-blue-600 focus:text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+                </svg>
+              </button>
+
+              <button type="button" className="flex items-center justify-center text-gray-400 transition-all duration-200 bg-transparent border border-gray-300 rounded w-9 h-9 hover:bg-blue-600 hover:text-white focus:bg-blue-600 focus:text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </section>
+
       </Layout>
     </>
 
