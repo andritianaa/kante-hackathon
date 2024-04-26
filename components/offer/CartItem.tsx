8fc6edcf4cbae6a8edf0b6a548f6c2ad82fca57f
Miandry05 <86937367+MiandryRakoto@users.noreply.github.com>
2024-04-26 16:23:26 +0300

    choco

--- components/offer/CartItem.tsx
+++ components/offer/CartItem.tsx
@@ -23,37 +23,37 @@ export const CartItem = (chocolate: Chocolate) => {
   };
 
   if(chocolate.occurences) 
-    return (
-      <div className="flex items-center w-full max-w-xl py-4 gap-4">
-        <img
-          className="w-20 h-20 object-cover rounded-lg"
-          src={
-            "https://thumbs.dreamstime.com/b/chocolat-avec-la-poudre-de-cacao-125953683.jpg"
-          }
-          alt="Neil image"
-        />
-        <div className="flex flex-col gap-1 items-start">
-          <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-            {chocolate.nom}
-          </p>
-          <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-            {chocolate.categorie}
-          </p>
-          <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-            {chocolate.prix} MGA
-          </p>
-        </div>
-        <div className="flex items-center">
-          <Button className="rounded-l-md" variant="outline" onClick={handleDec}>
-            <MinusIcon className="w-4 h-4" />
-          </Button>
-  
-          <Input type="number" value={chocolate.occurences} className="w-12" />
-          <Button className="rounded-r-md" variant="outline" onClick={handleInc}>
-            <PlusIcon className="w-4 h-4" />
-          </Button>
-        </div>
-        <Trash2 color="red" />
+  return (
+    <div className="flex items-center w-full max-w-xl py-4 gap-4">
+      <img
+        className="w-20 h-20 object-cover rounded-lg"
+        src={
+          "https://thumbs.dreamstime.com/b/chocolat-avec-la-poudre-de-cacao-125953683.jpg"
+        }
+        alt="Neil image"
+      />
+      <div className="flex flex-col gap-1 items-start">
+        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+          {chocolate.nom}
+        </p>
+        <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+          {chocolate.categorie}
+        </p>
+        <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+          {chocolate.prix} MGA
+        </p>
       </div>
-    );
+      <div className="flex items-center">
+        <Button className="rounded-l-md" variant="outline" onClick={handleDec}>
+          <MinusIcon className="w-4 h-4" />
+        </Button>
+
+        <Input type="number" value={chocolate.occurences} className="w-12" />
+        <Button className="rounded-r-md" variant="outline" onClick={handleInc}>
+          <PlusIcon className="w-4 h-4" />
+        </Button>
+      </div>
+      <Trash2 color="red" />
+    </div>
+  );
 };
