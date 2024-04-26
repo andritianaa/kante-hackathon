0c2d7904aa3c2d34f836d0d2aba3b87037f9e6d2
andritianaa <andritianasteve@gmail.com>
2024-04-26 16:37:50 +0300

    replace searchbar

--- app/(offer)/offer/page.tsx
+++ app/(offer)/offer/page.tsx
@@ -85,6 +85,7 @@ export default function RoutePage(props: PageParams<{}>) {
 
   return (
     <Layout className="mt-20">
+      <SearchBar onSearch={handleSearch}/>
       <div className="flex flex-wrap justify-center gap-3 pb-8">
         {filteredChocolates.map((chocolate) => (
           <ChocolateCard
