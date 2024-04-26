a4ceece1fffaab175e452db6d89e3c0377da7624
andritianaa <andritianasteve@gmail.com>
2024-04-26 08:50:19 +0300

    to TS

---
+++ lib/chocolates.ts
@@ -0,0 +1,112 @@
+export const chocolates = [
+    {
+        chocolat_id: 1,
+        nom: "Tablette Noir 70%",
+        description: "Delicieuse tablette de chocolat noir avec 70% de cacao.",
+        categorie: "Tablettes de chocolat",
+        origine: "C“te Ivoire",
+        prix: "38400.00",
+        image: "tablette_noir_70.png",
+        categorie_id: 1,
+        origine_id: 1
+    },
+    {
+        chocolat_id: 2,
+        nom: "Tablette Lait aux Noisettes",
+        description: "Tablette de chocolat au lait avec des eclats de noisettes croquants.",
+        categorie: "Tablettes de chocolat",
+        origine: "Venezuela",
+        prix: "24000.00",
+        image: "tablette_lait_noisettes.png",
+        categorie_id: 1,
+        origine_id: 2
+    },
+    {
+        chocolat_id: 3,
+        nom: "Praline Fondant",
+        description: "Praline fondant enrobe de chocolat noir.",
+        categorie: "Pralin‚s",
+        origine: "Equateur",
+        prix: "43200.00",
+        image: "praline_fondant.png",
+        categorie_id: 2,
+        origine_id: 3
+    },
+    {
+        chocolat_id: 4,
+        nom: "Truffe Chocolat Blanc",
+        description: "Truffe au chocolat blanc avec une touche de vanille.",
+        categorie: "Truffes",
+        origine: "Madagascar",
+        prix: "40000.00",
+        image: "truffe_chocolat_blanc.png",
+        categorie_id: 3,
+        origine_id: 4
+    },
+    {
+        chocolat_id: 5,
+        nom: "Chocolat Chaud epice",
+        description: "Melange de cacao avec une touche de cannelle et de piment.",
+        categorie: "Chocolats chauds",
+        origine: "P‚rou",
+        prix: "28000.00",
+        image: "chocolat_chaud_epice.png",
+        categorie_id: 4,
+        origine_id: 5
+    },
+    {
+        chocolat_id: 6,
+        nom: "Dragees aux Amandes",
+        description: "Dragees aux amandes enrobees de chocolat au lait.",
+        categorie: "Drag‚es",
+        origine: "Mexique",
+        prix: "32000.00",
+        image: "dragees_amandes.png",
+        categorie_id: 5,
+        origine_id: 6
+    },
+    {
+        chocolat_id: 7,
+        nom: "Barre Chocolat Caramel",
+        description: "Barre de chocolat au lait avec un cour fondant au caramel.",
+        categorie: "Barres de chocolat",
+        origine: "Ghana",
+        prix: "35000.00",
+        image: "barre_chocolat_caramel.png",
+        categorie_id: 6,
+        origine_id: 7
+    },
+    {
+        chocolat_id: 8,
+        nom: "Tablette Noir Intense",
+        description: "Tablette de chocolat noir avec 85% de cacao pour une experience intense.",
+        categorie: "Tablettes de chocolat",
+        origine: "C“te Ivoire",
+        prix: "39500.00",
+        image: "tablette_noir_intense.png",
+        categorie_id: 1,
+        origine_id: 1
+    },
+    {
+        chocolat_id: 9,
+        nom: "Praline Noisette",
+        description: "Praline fondant aux eclats de noisettes torrefiees.",
+        categorie: "Pralin‚s",
+        origine: "Venezuela",
+        prix: "44000.00",
+        image: "praline_noisette.png",
+        categorie_id: 2,
+        origine_id: 2
+    },
+    {
+        chocolat_id: 10,
+        nom: "Truffe Chocolat Noir",
+        description: "Truffe au chocolat noir riche et onctueuse.",
+        categorie: "Truffes",
+        origine: "Equateur",
+        prix: "35500.00",
+        image: "truffe_chocolat_noir.png",
+        categorie_id: 3,
+        origine_id: 3
+    }
+]
