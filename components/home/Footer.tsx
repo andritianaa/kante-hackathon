75b0c8e714c2020793d2a17cac76c5bcd5bff445
Manohisoaa <manohisoanirina707@mail.com>
2024-04-27 03:16:14 +0300

    lien page

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -71,7 +71,7 @@ export const Footer = () => {
         //         </div>
         //     </div>
         // </footer>
-        <section className="py-12 bg-orange-950 dark:bg-black">
+        <section className="py-10 bg-orange-950 dark:bg-black">
             <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
                 <div className="text-center xl:flex xl:items-center xl:justify-between xl:text-left">
                     <div className="xl:flex xl:items-center xl:justify-start">
@@ -83,11 +83,11 @@ export const Footer = () => {
                     <div className="items-center mt-8 xl:mt-0 xl:flex xl:justify-end xl:space-x-8">
                         <ul className="flex flex-wrap items-center justify-center gap-x-8 gap-y-3 xl:justify-end">
                             <li>
-                                <a href="#" title="" className="text-sm text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> A propos </a>
+                                <a href="/blog" title="" className="text-sm text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> A propos </a>
                             </li>
 
                             <li>
-                                <a href="#" title="" className="text-sm text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Nos produits </a>
+                                <a href="/offer" title="" className="text-sm text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Nos produits </a>
                             </li>
 
                             {/* <li>
@@ -101,7 +101,7 @@ export const Footer = () => {
 
                         <ul className="flex items-center justify-center space-x-8 xl:justify-end">
                             <li>
-                                <a href="#" title="" className="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80">
+                                <a href="https://twitter.com/zlatanrene/status/1241623461749035009" title="" className="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80">
                                     <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                                         <path
                                             d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z"
@@ -111,7 +111,7 @@ export const Footer = () => {
                             </li>
 
                             <li>
-                                <a href="#" title="" className="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80">
+                                <a href="https://web.facebook.com/p/Edenia-Chocolaterie-100063834920816/?_rdc=1&_rdr" title="" className="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80">
                                     <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                                         <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
                                     </svg>
@@ -119,7 +119,7 @@ export const Footer = () => {
                             </li>
 
                             <li>
-                                <a href="#" title="" className="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80">
+                                <a href="https://www.instagram.com/edeniachocolaterie?igsh=MW0wazY0c3o3eXRuMg==" title="" className="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80">
                                     <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                                         <path d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008z"></path>
                                         <circle cx="16.806" cy="7.207" r="1.078"></circle>
