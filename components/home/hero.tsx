3a514aae4a719678de9c422f150e2943f31e805a
andritianaa <andritianasteve@gmail.com>
2024-04-27 06:17:08 +0300

    okok

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "../ui/button";
 import { Layout } from "../layout";
+import { Swipper } from "./swipper";
 
 
 export function Hero() {
@@ -25,7 +26,7 @@ export function Hero() {
               </div>
             </div>
             <div className="lg:col-span-4 mt-10 lg:mt-0">
-              <img className="w-full rounded-xl" src="https://images.unsplash.com/photo-1665686376173-ada7a0031a85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=900&h=700&q=80" alt="Image Description" />
+              <Swipper/>
             </div>
           </div>
         </div>
